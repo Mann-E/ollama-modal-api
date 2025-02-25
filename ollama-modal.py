dcef81321852e3abef0b735fa6254ded61ede42b
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-02-25 18:41:06 +0330

    going to a less expensive option

--- ollama-modal.py
+++ ollama-modal.py
@@ -5,7 +5,7 @@ import time
 
 from modal import build, enter, method
 
-MODEL = os.environ.get("MODEL", "mistral-large")
+MODEL = os.environ.get("MODEL", "mistral-small")
 
 
 def pull(model: str = MODEL):
@@ -36,7 +36,7 @@ with image.imports():
     import ollama
 
 
-@app.cls(gpu="h100:4", container_idle_timeout=300)
+@app.cls(gpu="l40s", container_idle_timeout=300)
 class Ollama:
     @build()
     def pull(self):
