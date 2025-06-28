40625f235d9d05da6776833c032f3752250420a7
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-06-28 16:14:50 +0330

    added qwen3 model

--- ollama-modal.py
+++ ollama-modal.py
@@ -8,7 +8,7 @@ flavor = "devel"
 operating_sys = "ubuntu22.04"
 tag = f"{cuda_version}-{flavor}-{operating_sys}"
 
-MODEL = os.environ.get("MODEL", "gemma3:27b")
+MODEL = os.environ.get("MODEL", "qwen3:14b")
 
 # Function to initialize and pull the model
 def pull_model(model: str = MODEL):
